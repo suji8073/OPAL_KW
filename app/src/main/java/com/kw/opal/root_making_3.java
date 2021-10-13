fe3ac90928327b36f0a59f5b776da2725e85d416
suji8073 <suzy8073@naver.com>
2021-10-13 18:53:23 +0900

    1013

--- app/src/main/java/com/kw/opal/root_making_3.java
+++ app/src/main/java/com/kw/opal/root_making_3.java
@@ -22,16 +22,17 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class root_making_3 extends root_making {
-    public root_making_3(){
-        this.layout =R.layout.root_making_3;
-        this.userview=R.id.userListTextView3;
+    public root_making_3() {
+        this.layout = R.layout.root_making_3;
+        this.userview = R.id.userListTextView3;
         this.cartv = R.id.cart;
         this.finishv = R.id.finish3;
         this.table = "hotel";
-        this.catlist= new ArrayList<>(Arrays.asList("all","A03020200","B02010100","B02010200","B02010300","B02010400","B02010500","B02010600","B02010700","B02010800","B02010900","B02011000","B02011100","B02011200","B02011300","B02011400","B02011500","B02011600"));
-        this.c_one=new Button[5];
+        this.catlist = new ArrayList<>(Arrays.asList("all", "A03020200", "B02010100", "B02010200", "B02010300", "B02010400", "B02010500", "B02010600", "B02010700", "B02010800", "B02010900", "B02011000", "B02011100", "B02011200", "B02011300", "B02011400", "B02011500", "B02011600"));
+        this.c_one = new Button[5];
         this.category = new Integer[]{R.id.one_1, R.id.one_2, R.id.one_3, R.id.one_4, R.id.one_5};
 
         this.on_off = new int[catlist.size()];
     }
+}
 
