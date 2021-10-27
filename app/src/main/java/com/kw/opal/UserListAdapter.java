eb2b41f0b3e2843acc466a5b5e30c0112742b621
Chi-Square <luckykr1234@gmail.com>
2021-10-27 15:07:14 +0900

    대격변

--- app/src/main/java/com/kw/opal/UserListAdapter.java
+++ app/src/main/java/com/kw/opal/UserListAdapter.java
@@ -143,6 +143,7 @@ public class UserListAdapter extends BaseAdapter {
                 three = String.valueOf(pointList.get(position).getImage());
                 Float x = Float.valueOf(pointList.get(position).getMap_x());
                 Float y = Float.valueOf(pointList.get(position).getMap_y());
+                String typeid=String.valueOf(pointList.get(position).getContentTypeId());
                 set.addAll(Collections.singleton(one));
                 set.addAll(Collections.singleton(two));
                 set.addAll(Collections.singleton(three));
@@ -150,7 +151,7 @@ public class UserListAdapter extends BaseAdapter {
 
                 helper.open();
 
-                helper.insertColumn(id1, one, three,two,x,y,area_name,null);
+                helper.insertColumn(id1, one, three,two,x,y,area_name,typeid);
 
                 count++;
             }
